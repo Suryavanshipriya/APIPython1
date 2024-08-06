ef4a7e4cb01c1c4003e074206c273f15c629dc30
Priyanka <nimbalkarpriyanka0@gmail.com>
2024-08-06 16:35:06 +0100

    changesforjenkins

--- src/dbapi.py
+++ src/dbapi.py
@@ -2,8 +2,6 @@ from urllib.parse import quote_plus
 import pandas as pd
 from sqlalchemy import create_engine
 from flask import Flask, jsonify
-import pandas as pd
-from sqlalchemy import create_engine
 
 app = Flask(__name__)
 # Define your PostgreSQL connection parameters
@@ -17,30 +15,21 @@ PORT = 5432  # Default PostgreSQL port
 DATABASE = 'testdb'  # Replace with your PostgreSQL database name
 
 # Create a SQLAlchemy engine
-#engine = create_engine(f'{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}')
-engine = create_engine('{0}+{1}://{2}:{3}@{4}:{5}/{6}'.format(
-    DATABASE_TYPE, DBAPI, USER, PASSWORD, ENDPOINT, PORT, DATABASE))
+engine = create_engine(f'{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}')
 
 # Specify the table name you want to read from
 table_name = 'customer_Priya'  # Replace with your table name
-
-# Read data from the PostgreSQL table into a DataFrame
-#df = pd.read_sql_table(table_name, engine)
-
-# Print the DataFrame to verify
-#print(df.head())
-
 
 @app.route('/data', methods=['GET'])
 def get_data():
     # Read data from the PostgreSQL table into a DataFrame
     df = pd.read_sql_table(table_name, engine)
-    
+
     # Convert the DataFrame to a dictionary and then to JSON
     data = df.to_dict(orient='records')
-    
+
     # Return the data as JSON
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
